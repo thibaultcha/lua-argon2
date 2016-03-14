022accecb1201c6ff0f116634109116495d470a8
Thibault Charbonnier <thibaultcha@me.com>
2016-03-14 18:46:14 -0400

    style(ci) indent pushd bash script sections

--- .ci/setup_argon2.sh
+++ .ci/setup_argon2.sh
@@ -4,8 +4,8 @@ set -e
 
 git clone https://github.com/P-H-C/phc-winner-argon2.git $ARGON2_DIR
 pushd $ARGON2_DIR
-git checkout $ARGON2_VERSION
-make
-make test
+  git checkout $ARGON2_VERSION
+  make
+  make test
 popd
 
