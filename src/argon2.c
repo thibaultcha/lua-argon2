3eb139ad5779c9dcf1c0249a730885c3c5b7b847
Thibault Charbonnier <thibaultcha@me.com>
2016-04-02 18:00:49 -0700

    doc: fix mention of borrowed code

--- src/argon2.c
+++ src/argon2.c
@@ -281,7 +281,7 @@ static int largon2_verify(lua_State *L) {
 /* Compatibility for Lua 5.1.
  *
  * luaL_setfuncs() is used to create a module table where the functions have
- * json_config_t as their first upvalue. Code borrowed from Lua 5.2 source. */
+ * largon2_config as their first upvalue. Code borrowed from Lua 5.2 source. */
 static void luaL_setfuncs(lua_State *l, const luaL_Reg *reg, int nup) {
     int i;
 
